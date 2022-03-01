df682812262ab837b6aa8edb0b4395a6f019f857
Gunnar Torfi Steinarsson <gunnartorfis@gmail.com>
2022-03-01 22:39:14 +0000

    First commit

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
 import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'react-native-week-month-date-picker';
+import { DatePicker, multiply } from 'react-native-week-month-date-picker';
 
 export default function App() {
   const [result, setResult] = React.useState<number | undefined>();
@@ -13,6 +13,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Result: {result}</Text>
+      <DatePicker date={new Date()} />
     </View>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,16 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
 export function multiply(a: number, b: number): Promise<number> {
-  return Promise.resolve(a * b);
+  return Promise.resolve(a * b + 2);
 }
+
+export const DatePicker: React.FC<{
+  date: Date;
+}> = ({ date }) => {
+  return (
+    <View>
+      <Text>Date: {date.toString()}</Text>
+    </View>
+  );
+};
